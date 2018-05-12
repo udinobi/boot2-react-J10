15a3222b1780516562e3e8bf9f094b51d59096b9
Lucio Biondi <luke@wlc-asean.com>
2018-05-12 17:15:28 +0700

    Initial commit

---
+++ backend/src/main/java/com/wlc/boot2vueJ10/Boot2VueJ10Application.java
@@ -0,0 +1,12 @@
+package com.wlc.boot2vueJ10;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Boot2VueJ10Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Boot2VueJ10Application.class, args);
+	}
+}
